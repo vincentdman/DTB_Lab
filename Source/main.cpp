facc604ca7c353f5a79e881fc8f288966f00c5a3
vincentdman <vincent1de1man@gmail.com>
2022-06-03 22:49:43 +0200

    dtb update

--- Source/main.cpp
+++ Source/main.cpp
@@ -25,7 +25,14 @@ const std::string delimiter = ", ";
 long long int SampleNumber = 0;
 const short int Resolution = 16; 
 
-
+static int callback(void* NotUsed, int argc, char** argv, char** azColName){
+    int i;
+    for(i = 0; i<argc; i++){
+        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+    }
+    printf("\n");
+    return 0;
+}
 SenseHAT SensHat;
 /**
  * @brief Main function of roomba code. 
@@ -53,10 +60,11 @@ void MQTT_Updater(int Interval = 5)
 {
     while(1)
     {
-    MqttHandler.publishInfo(std::to_string(NodeID) + delimiter + std::to_string(SampleNumber) + delimiter + "'Temperature'" + delimiter + std::to_string(static_cast<int>(SensHat.get_temperature()) )  + delimiter + "'C'" + delimiter + std::to_string(Resolution) , "DataBaseMV/Temperature");
-    MqttHandler.publishInfo(std::to_string(NodeID) + delimiter + std::to_string(SampleNumber) + delimiter + "'Humidity'" + delimiter + std::to_string(static_cast<int>(SensHat.get_humidity()) )  + delimiter + "'%rh'" + delimiter + std::to_string(Resolution) , "DataBaseMV/Humidity");
-    MqttHandler.publishInfo(std::to_string(NodeID) + delimiter + std::to_string(SampleNumber) + delimiter + "'Pressure'" + delimiter + std::to_string(static_cast<int>(SensHat.get_pressure()) )  + delimiter + "'hPa'" + delimiter + std::to_string(Resolution) , "DataBaseMV/Pressure");
-    
+    MqttHandler.publishInfo(std::to_string(NodeID) + delimiter + std::to_string(NodeID) + std::to_string(SampleNumber) + delimiter + "'Temperature'" + delimiter + std::to_string(static_cast<int>(SensHat.get_temperature()) )  + delimiter + "'C'" + delimiter + std::to_string(Resolution) , "DataBaseMV/Temperature");
+    SampleNumber++;
+    MqttHandler.publishInfo(std::to_string(NodeID) + delimiter + std::to_string(NodeID) + std::to_string(SampleNumber) + delimiter + "'Humidity'" + delimiter + std::to_string(static_cast<int>(SensHat.get_humidity()) )  + delimiter + "'%rh'" + delimiter + std::to_string(Resolution) , "DataBaseMV/Humidity");
+    SampleNumber++;
+    MqttHandler.publishInfo(std::to_string(NodeID) + delimiter + std::to_string(NodeID) + std::to_string(SampleNumber) + delimiter + "'Pressure'" + delimiter + std::to_string(static_cast<int>(SensHat.get_pressure()) )  + delimiter + "'hPa'" + delimiter + std::to_string(Resolution) , "DataBaseMV/Pressure");
     SampleNumber++;
     sleep(Interval);
     }
@@ -88,17 +96,23 @@ void DataBase()
         bool node1 = false;
         bool node2 = false;
 
-        while (!node1 && !node2)
+        while (!(node1 && node2))
         {
             if (PressureGetter.get_flag())
             {
+                temp.clear();
+                hum.clear();
+                pres.clear();
+                
                 temp = TemperatureGetter.get_received_command();
+               std::cout<<temp[0]<<std::endl; 
                 hum = HumidityGetter.get_received_command();
                 pres = PressureGetter.get_received_command();
             }
 
             if (temp[0] == '1')
             {
+                //std::cout<<"1 done!\n\n";
                 temp1 = temp;
                 hum1 = hum;
                 pres1 = pres;
@@ -109,6 +123,7 @@ void DataBase()
             }
             if (temp[0] == '2')
             {
+                //std::cout<<"2 done!\n\n";
                 temp2 = temp;
                 hum2 = hum;
                 pres2 = pres;
@@ -118,15 +133,15 @@ void DataBase()
             }
         }
 
-        //sqlite3_open("DataBaseMV.db", &db);
+        sqlite3_open("../DataBaseStorage/DataBaseMV.db", &db);
 
         auto current = std::chrono::system_clock::now();
         std::time_t current_time = std::chrono::system_clock::to_time_t(current);
         tm* tm_local = localtime(&current_time);
 
         std::string sql = "INSERT INTO timestamp_measurement(UNIX_TIME, current_hour, current_minute, current_second, current_day, current_year) VALUES(" + std::to_string(current_time) + "," + std::to_string(tm_local->tm_hour) + "," + std::to_string(tm_local->tm_min) + "," + std::to_string(tm_local->tm_sec) + "," + std::to_string(tm_local->tm_yday) + "," + std::to_string(tm_local->tm_year) + ");" \
-            "INSERT INTO node(NODE_ID, location, UNIX_TIME) VALUES(" + NODE_ID1 + "," + location1 + "," + std::to_string(current_time) + "); " \
-            "INSERT INTO node(NODE_ID, location, UNIX_TIME) VALUES(" + NODE_ID2 + "," + location2 + "," + std::to_string(current_time) + "); " \
+            "INSERT INTO node(NODE_ID, location, UNIX_TIME) VALUES(" + NODE_ID1 + "," + "'" + location1 + "'" + "," + std::to_string(current_time) + "); " \
+            "INSERT INTO node(NODE_ID, location, UNIX_TIME) VALUES(" + NODE_ID2 + "," + "'" + location2 + "'" + "," + std::to_string(current_time) + "); " \
             "INSERT INTO sample(NODE_ID, SAMPLE_NO, measured_variable, value, unit, resolution) VALUES(" + temp1 + ");" \
             "INSERT INTO sample(NODE_ID, SAMPLE_NO, measured_variable, value, unit, resolution) VALUES(" + hum1 + ");" \
             "INSERT INTO sample(NODE_ID, SAMPLE_NO, measured_variable, value, unit, resolution) VALUES(" + pres1 + ");" \
@@ -137,9 +152,9 @@ void DataBase()
         std::cout << sql << std::endl;
 
 
-    //sqlite3_exec(db, sql, callback, 0, &zErrMsg);
-    //sqlite3_close(db);
-    //node1 = false;
-    //node2 = false;
+    sqlite3_exec(db, sql.c_str(), callback, 0, &zErrMsg);
+    sqlite3_close(db);
+    node1 = false;
+    node2 = false;
     }
 }
